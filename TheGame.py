9ede5bea0fdbea685303a1bdca952fa21185e518
MILLION BERHAN <99065113@mydavinci.com>
2021-09-28 13:58:04 +0200

    F1.3.01.O2 - Maak je eigen game

---
+++ TheGame.py
@@ -0,0 +1,146 @@
+print(                      
+"""                         
+████████╗██╗░░██╗███████╗  ░██████╗░░█████╗░███╗░░░███╗███████╗  ░█████╗░███████╗  ████████╗██╗░░██╗███████╗
+╚══██╔══╝██║░░██║██╔════╝  ██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔══██╗██╔════╝  ╚══██╔══╝██║░░██║██╔════╝
+░░░██║░░░███████║█████╗░░  ██║░░██╗░███████║██╔████╔██║█████╗░░  ██║░░██║█████╗░░  ░░░██║░░░███████║█████╗░░
+░░░██║░░░██╔══██║██╔══╝░░  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██║░░██║██╔══╝░░  ░░░██║░░░██╔══██║██╔══╝░░
+░░░██║░░░██║░░██║███████╗  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ╚█████╔╝██║░░░░░  ░░░██║░░░██║░░██║███████╗
+░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ░╚════╝░╚═╝░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝
+
+░█████╗░███████╗███╗░░██╗████████╗██╗░░░██╗██████╗░██╗░░░██╗
+██╔══██╗██╔════╝████╗░██║╚══██╔══╝██║░░░██║██╔══██╗╚██╗░██╔╝
+██║░░╚═╝█████╗░░██╔██╗██║░░░██║░░░██║░░░██║██████╔╝░╚████╔╝░
+██║░░██╗██╔══╝░░██║╚████║░░░██║░░░██║░░░██║██╔══██╗░░╚██╔╝░░
+╚█████╔╝███████╗██║░╚███║░░░██║░░░╚██████╔╝██║░░██║░░░██║░░░
+░╚════╝░╚══════╝╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░
+
+░█████╗░░░░░██╗██████╗░██████╗░  ██████╗░░█████╗░██████╗░░░███╗░░
+██╔══██╗░░░██╔╝╚════██╗╚════██╗  ╚════██╗██╔══██╗╚════██╗░████║░░       
+╚██████║░░██╔╝░░░███╔═╝░░███╔═╝  ░░███╔═╝██║░░██║░░███╔═╝██╔██║░░
+░╚═══██║░██╔╝░░██╔══╝░░██╔══╝░░  ██╔══╝░░██║░░██║██╔══╝░░╚═╝██║░░
+░█████╔╝██╔╝░░░███████╗███████╗  ███████╗╚█████╔╝███████╗███████╗
+░╚════╝░╚═╝░░░░╚══════╝╚══════╝  ╚══════╝░╚════╝░╚══════╝╚══════╝ 
+          """)
+print("""",*.......,*,,,*,,,,*,*##&@@@@@@@@@@&&@&&&&&&&&&&&@&&@&&@&@@&@@%&@@&@@&%&%@%%&&&&
+,*,.......,**,,,,,,*,**(%&@@@@@@@@@&@@&@&&&@@@&&&@&@&&@&@@@#&&@@&@@@@&&&&%@@#@&@
+,,,.......,,,,,,,,,,,**/%&&@@&&@&@@&&%&&%@@&@@@@&&%&&&@@@@&&@@@@&%%%&@@@@&&&&&&@
+,,,,..,,,,,,,,,,,,,,......**,@@@@@@@(////**/@%&%&&%%#&&@&@@&@&/%%#%&%&&#&&@@&&%%
+,,,,,,,,,,,,,,,,***, .   .,,@%@@(&&#//***,,**%#&%&&&%&(&&@@%&&@@&&@@&&@@@&&%%&@&
+,,,,,,,,,,,,,,,,,***,,,,,,,..**%#@###%/*###*/(,#&%&&&&%*#%&&&&&&&@@@@&%&@@&&&&%&
+,,,,,,,,,,,,,,,,,,,*,,,,,.....,%@@#%((*/*(##(,**(%%&&%&&##/,.....,(&&&&%&&@%%%&&
+,.......,,,,,,,,,,,,,,,........%@(/*(%%#**,,,*/#&*(#%&@&&&(*,...................
+,/..........,,,,.,,,,,,.......*%%(((####(**//(((%&%&@(//@&&&*...................
+,, / ........,,...............#*%@((#%%(/**////(%&%@@%@#@@&(*...................
+.,,***................   ...(&&#&&@##((((###%%%&&*,#&@@&&%#&&...................
+.,.*../.........,,.,... .*..@&%##@&@@@&%%%###%#(#@*%#&&&#&&#&...................
+**,.... .,,,,.///*///*/*,...,%#%&&&@(@%%%##///*/%&(@@%&%&&&&%...................
+**.,..*/****../////...*,.....&%@&@*@@@%%##//***/(/%@@@@%%#%#&/..................
+.  . .,,,,,,,,,,,,,,,.     .&*/&&(@@&%%(/******//***/(&&@&%&%(..................
+     ..,,,,,,,,,,,..,,,,,,,,,((((///#/////******//***(/**********/..............
+,,,,,,,,,,,,,,,,,..,,,*///*******//(,,***,*****,******(******,,***/.............
+,,,,,,,,,,,,,,,,,,,,((//**,****/**(**********,*********/***////((##(............
+.,,,,,,,,,,,,.,...(((((((////////(*********,,,,********/****//(######...........
+,,,,.,,,,,,..,...((((((##(((/////(////*****,,***/****//(((*//((%#####...........
+,,..,.........,.#((((#####((///(((((*********//******/(((/((*/%######...........
+..............(########%%&%(/(((((((((*,,****/***,,*((((((/((/#%%%%%#...........
+.............(#######%%&%..(/(((((((((#(,****//***((((((((((((&%%####...........
+.........../########%&......((#(((###(#(##//,,///%#(((###((((#&&%%###,..........
+..........((#####%%%.........##(&%###(##%#%(&&/(##(((%#(/%(%&.%%&%%%##..........
+.........((((##%&&............###((((///***((((((,**////((##...%%%%###.. .....  
+.........#(////#&.............#((((((///***(((((((***////((,.. %#(/**/.         
+..........(///*/(#............#((((((///**(((&(((((*////(((...(((/***/          
+...........(//**/(#...........#(((((((((/#(((/#%%(((,////(#.  (((/**//          
+............#(//*/((..........##(#(((((((((((/((((/////((#    ((/**//#..........
+.............#(//*//(*........##((##((((((#((((((((((####%.,..(/**//(*,,,,,,,,,,
+...............#(/***//......#(((((((((((((%(((((((#######/(((//**/(............
+.................#//**//((((/(((((((((((((((%%((((((##((##@(((((,*/,............
+...................(***//#(%#((((((((((//////#///((((((###(##@%*,*(,,,,,,,,.,,,,
+.................    #/,#(((##((%(((((((///((///(((((((##(((#((&**,,,,,,,,,,,,,,
+..........          . /%#(((//((((#####(((((((((((((((#((((((#((,,,,,,,,,,,,,,,,
+                   ../#((((((//(/(%(#################(&#((((###%#*,,,,,,,,,,,,,,
+............... ....*##%(%%(/(((((((%(############%(&##((((((((#%#*,,,,,,,,,,,,,
+.....................(((%%(((((((((((#&((%%%%%%%((&%###(((((((((#%#,,,,,,,,,,,,,
+......................*##(((//((((((((%%&%#&%%%%%%%###((((//(((((##**,,,,*******
+...................,,,(#((((//////((#((%%%%%%%%%%&#(((((//**////((#*,*,*,*******
+.............,,,,,,,,,((((((////////((#%%%%%%%%%&%(((((///**/////((****,********
+........,,,,,,,,,,(///(#(((((//////(((###&*****&##((((////***//((((*************
+...,,,,,,,,,,*//((((((/##((((//////(((####%****###((((///****//(((#(************
+,***,,,**(//(/((((//////###(((/////(((###%%****###(((((//****//(((#*************
+****((((((((((///////////##((((////(((###%%/***%##(((((///***//(((#*************
+/(((((((((////////////***###(((((((((####%%*****%###((((///**///((#*************
+##((((////////////********###(((((((####%%%*****%####((((//**//(((#***********( """)
+
+
+
+print ("""
+█░█ █▀▀ ▀█▀   █░█ █▀▀ █▀█ █░█ ▄▀█ ▄▀█ █░░   █░█ ▄▀█ █▄░█   █▀▀ █▀▀ █▄░█   █▀ █░░ █▀▀ █▀▀ █░█ ▀█▀ █▀▀   █▀▄ ▄▀█ █▀▀
+█▀█ ██▄ ░█░   ▀▄▀ ██▄ █▀▄ █▀█ █▀█ █▀█ █▄▄   ▀▄▀ █▀█ █░▀█   ██▄ ██▄ █░▀█   ▄█ █▄▄ ██▄ █▄▄ █▀█ ░█░ ██▄   █▄▀ █▀█ █▄█
+
+█▀█ █▀█   █▀▄ █▀▀   █░█ █ █▀▀ █░█   █▀ █▀▀ █░█ █▀█ █▀█ █░░ █
+█▄█ █▀▀   █▄▀ ██▄   █▀█ █ █▄█ █▀█   ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄ ▄""")
+
+A = "A"
+
+name = input ("wat is je naam?")
+leeftijd = int(input("hoe oud ben je?"))
+
+if leeftijd <18:
+    print(f"Beste {name}, je bent helaas te jong om het spel te spelen!")
+else:
+    print(f"Beste {name}, u bent oud genoeg om het spel te mogen spelen!")
+
+    startvraag = input("Er is een gek die met wapens op leerlingen schiet, hij is op dit moment in de kantine, jij zit in het IT lokaal. Wat ga jij doen? ")
+    print ("A: rent via de nooduitgang naar buiten. ")
+    print ("B: blijft in het lokaal en gaat verstoppen.")
+
+    if startvraag == "A":
+        print("Je rent naar buiten en je stopt met rennen op een gegeven moment, wat ga je doen?")
+        print("A: Je belt de politie. ")
+        print("B: je gaat hem proberen aan te vallen als hij omkijkt.")
+    else:
+        print("Je blijft in het lokaal en gaat je verstoppen, je ziet dat de dader met het wapen gaat kijken of er mensen in je lokaal zitten, wat ga jij doen?")
+        print("A: je blijft cool, calm, collective net Selma Omari.")
+        print("B: wees dom en gek net famke louise en rent rondjes door het lokaal.")
+
+    eerstelinksaroute = input ("je gaat hem proberen aan te vallen wat ga je doen?")
+    print ("A: je gaat hem met de vuist proberen te pakken, zodat je de held van de eeuw kunt gaan worden.")
+    print("B: Je besluit toch terug te trekken en je gaat als een mietje in een hoekje huilen.")
+
+    if eerstelinksaroute == "A":
+        print ("Je belt de politie en twijfelt tussen 2 nummers, welk nummer besluit jij te bellen?")
+        print ("B:112")
+        print ("B:911")
+    else:
+        print ("Je gaat hem proberen aan te vallen wat ga je doen? ")
+        print ("A: een goeie op zijn linker!")
+        print ("B: een goeie op zijn rechter!")
+
+    eersteabroute = input ("Je gaat hem proberen aan te vallen wat ga je doen?\nA: je gaat hem met de vuist proberen te pakken, zodat je de held van de eeuw kunt gaan worden!\nB: Je besluit toch terug te trekken en je gaat als een mietje in een hoekje huilen.")
+
+    if eersteabroute == "A":
+        print("Je besluit om je terug te trekken en je gaat in een hoekje huilen, wat besluit je te doen als de schutter naar je toe rent en niet besluit te schieten?")
+        print("A: je blijft als een mietje huilen en hoopt dat hij niet op jou gaat schieten.")
+        print("B: je besluit om als een gek weg te rennen en riskeert dat hij gaat schieten!")
+
+    if eersteabroute == "A":
+        print("Politie is optijd aangekomen en je bent in veiligheid gebracht en de dader is opgepakt!")
+    else:
+        print("je bent ten gevolge van je laatste keuze overleden, R.I.P")
+
+    startvraagrechts = input ("je blijft in het lokaal en gaat je verstoppen, je ziet dat de dader met het wapen gaat kijker of er mensen in je lokaal zitten, wat ga jij doen?")
+    if startvraagrechts == "A":
+        print("je bent cool, calm, collective gebleven net Selma, ziet dat de shooter uit het lokaal rent, wat ga jij doen?")
+    else:
+        print("je gaat als een gek rondjes rennen, maar je bedenkt je dat het niet het slimste is wat je hebt gedaan, wat ga je nu doen? want de schutter komt steeds dichterbij! ")
+
+    startvraagrechtsrechts = input ("je bent cool, calm, collective gebleven net Selma, je ziet dat de shooter uit het lokaal rent, wat ga je doen?\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel.\nB: je gaat spullen verzamelen om op het te gooien of om jezelf te beschermen. ").lower() == "a"
+
+    if startvraagrechtsrechts == "A":
+        print ("je hebt de aanslag overleefd!")
+    else:
+        print ("U bent helaas ten gevolgen van uw beslissingen het niet overleefd!")
+
+    if input("je gaat als een gek rondjes rennen, maar je bedenkt je dat het niet het slimste is wat wat je hebt gedaan wat ga je nu doen? Want de schutter komt dichterbij?\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel. \nB: je gaat spullen verzamelen om op het te gooien of om jezelf te beschermen. ").lower() == "a" :
+        print("U heeft helaas verloren!")
+    else:
+        print ("U heeft helaas verloren!")
---
+++ TheGame.py bijna af.py
@@ -0,0 +1,157 @@
+print(                      
+"""                         
+████████╗██╗░░██╗███████╗  ░██████╗░░█████╗░███╗░░░███╗███████╗  ░█████╗░███████╗  ████████╗██╗░░██╗███████╗
+╚══██╔══╝██║░░██║██╔════╝  ██╔════╝░██╔══██╗████╗░████║██╔════╝  ██╔══██╗██╔════╝  ╚══██╔══╝██║░░██║██╔════╝
+░░░██║░░░███████║█████╗░░  ██║░░██╗░███████║██╔████╔██║█████╗░░  ██║░░██║█████╗░░  ░░░██║░░░███████║█████╗░░
+░░░██║░░░██╔══██║██╔══╝░░  ██║░░╚██╗██╔══██║██║╚██╔╝██║██╔══╝░░  ██║░░██║██╔══╝░░  ░░░██║░░░██╔══██║██╔══╝░░
+░░░██║░░░██║░░██║███████╗  ╚██████╔╝██║░░██║██║░╚═╝░██║███████╗  ╚█████╔╝██║░░░░░  ░░░██║░░░██║░░██║███████╗
+░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  ░╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚══════╝  ░╚════╝░╚═╝░░░░░  ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝
+
+░█████╗░███████╗███╗░░██╗████████╗██╗░░░██╗██████╗░██╗░░░██╗
+██╔══██╗██╔════╝████╗░██║╚══██╔══╝██║░░░██║██╔══██╗╚██╗░██╔╝
+██║░░╚═╝█████╗░░██╔██╗██║░░░██║░░░██║░░░██║██████╔╝░╚████╔╝░
+██║░░██╗██╔══╝░░██║╚████║░░░██║░░░██║░░░██║██╔══██╗░░╚██╔╝░░
+╚█████╔╝███████╗██║░╚███║░░░██║░░░╚██████╔╝██║░░██║░░░██║░░░
+░╚════╝░╚══════╝╚═╝░░╚══╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░
+
+░█████╗░░░░░██╗██████╗░██████╗░  ██████╗░░█████╗░██████╗░░░███╗░░ 
+██╔══██╗░░░██╔╝╚════██╗╚════██╗  ╚════██╗██╔══██╗╚════██╗░████║░░       
+╚██████║░░██╔╝░░░███╔═╝░░███╔═╝  ░░███╔═╝██║░░██║░░███╔═╝██╔██║░░
+░╚═══██║░██╔╝░░██╔══╝░░██╔══╝░░  ██╔══╝░░██║░░██║██╔══╝░░╚═╝██║░░
+░█████╔╝██╔╝░░░███████╗███████╗  ███████╗╚█████╔╝███████╗███████╗
+░╚════╝░╚═╝░░░░╚══════╝╚══════╝  ╚══════╝░╚════╝░╚══════╝╚══════╝ 
+          """)  
+print ("""@@@@@@@@@@@@@@@@@@@@@@@@@@.,,.@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@.,,,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@***(((((/,,,,,,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@*******(((((((((((((((((((,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@@@@@
+@@@@@@@@@@@@@@@**********((#######################//,,,,,,,,,,,,,,,,,,,/@@@@@@@@
+@@@@@@@@@@@@**********(###############################//,,,,,,,,,,,,,,,,,,@@@@@@
+@@@@@@@@@*********/((###################################////,,,,,,,,,,,,,,,@@@@@
+@@@@@@@*********(((#######################################(///,,,,,,,,,,,,,,,@@@
+@@@@@*********((((##########################################/////,,,,,,,,,,,,,@@
+@@@@********(((((#############################################////,,,,,,,,,,,,,@
+@@@*******(((((((##############################################/////,,,,,,,,,,,,
+@@*******(((((((#   ###########  *###  #  ################   ###/////,,,,,,,,,,,
+@*******((((((((#   ############  ##  (#########################//////,,,,,,,,,,
+@*******(((((  (#   #####   ####  ,#  ##  ##  ###  #   ###  .####//////,,,,,,,,,
+@******(((((   (#   #  (#   #####    ###  ##  ###  #  ####  .####//////,,,,,,,,,
+*******((((((       #   /   ######  ####  ##  ###  ##    #  .####//////,,,,,,,,,
+@******((((((((((###################################  # #########//////,,,,,,,,.
+@******((((((((((########################### ### # #  # ## # # # ,/ / /,,,,,,,,@
+@******(((((((((((##########################  (#   #  # ##   #   ,/   /,,,,,,,@@
+@@******((((((((((((###########################################////////,,,,,,@@@
+@@@*****(((((((((((((#########################################////////,,,,,@@@@@
+@@@@*****((((((((((((((######################################////////,,,,#@@@@@@
+@@@@@@****/((((((((((((((###################################////////,,,@@@@@@@@@
+@@@@@@@@****((((((((((((((((##############################////////,,,@@@@@@@@@@@
+@@@@@@@@@@****(((((((((((((((((#########################/////////@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@***((((((((((((((((((((#################///////,,@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@***((((((((((((((((((((((((((((((,,,,,,,,,,,@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@*((((((((((((((((((((((,,,,,,,,,,,,@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@,,,,,,,,,,,,,,,,,,,,,,,,@@@@@@@@@@@@@@@@@@@ """)
+print("""                                                                          
+,                                                  .  ...........,**,.      ....
+*,,,,,,,,,,,,,,,,,,,..,,,,,,,,,,,,,,,,******/(((((/****,*/((((((((#####%%%%%%#(*
+/*//((((((((///(((((///////////////*     .. (.*,(/(////((((((((((((((((//(((((((
+/*/////////////////////**/////////*#/(((((/*/*/*///////((//////(//////////////((
+/,*//////////****/*//((************, .   .  ,   ***...,*/***/////////////****///
+/,,////////*****/(***((***********,,/*.  */*/**/((//////////////////////******//
+/,,****/********/((**((,,,,,,,,,,,,/(***.*/////*//////*///**///////***//,,,,**//
+*,,,*********,,,,##**((*..,,,,,,...(((//,.///////////((///***/////******,,,,****
+*,..********,,,,,(((**#* ..........*(////,,////////**///(/*///////******,,,,,***
+*....******,,,,,,(((,,(/ .........../#////..(////////***///((/////******,,.,,***
+&%,..,*****,,,,,,,,,.,//........... (/,((#((////##(/////##(///#(/////##.,%##(,,*
+%##  .,,,,,,,,,,**,,,*/##(((((((((//((((###/(////###//////###///(#(/(/*#,#(%###*
+%##.  ,,,,,,,,,,,,***,/((////////***///////***/////////////////////(%/(*#(((###(
+%###  .*,.,,,,,,,,,(/(/((/////////////////////*////////////////////////(%%%%####
+%###    /,,,,,,,,,,((/////////////////////////////////////////////////*####(#(##
+%####   .,..,.,,,,,((//////////////////////////////////////////////////%####(((/
+%####    /...,,,,/(////////*/********///////**,,.,**//////**/////////####/##(***
+%#####   .......(//////////*****/****/*/*************************///(##*//###*//
+%#####    ....,(///******,,,.....      ...........................,,%#.....,/##,
+%#####%  ..../(####%%%&&&&&&&&&&%%%%%################################(##########
+,######.  ..(##################################(/////########(########(##(((((##
+//#####%  .(%#####(((#####((((#(#(###((((#((##((*,,*,*((#(#(((((((((((((((((((((
+, ######,  ######(((((#((((((((((((((((((((((((((*,,,,((((((((((((((((((((((((((
+%#%#%%%##  *####((((((((((((((((((((((((((((((((((((((##(((((((((((((((((((((((( """)
+
+print ("""
+█░█ █▀▀ ▀█▀   █░█ █▀▀ █▀█ █░█ ▄▀█ ▄▀█ █░░   █░█ ▄▀█ █▄░█   █▀▀ █▀▀ █▄░█   █▀ █░░ █▀▀ █▀▀ █░█ ▀█▀ █▀▀   █▀▄ ▄▀█ █▀▀
+█▀█ ██▄ ░█░   ▀▄▀ ██▄ █▀▄ █▀█ █▀█ █▀█ █▄▄   ▀▄▀ █▀█ █░▀█   ██▄ ██▄ █░▀█   ▄█ █▄▄ ██▄ █▄▄ █▀█ ░█░ ██▄   █▄▀ █▀█ █▄█
+
+█▀█ █▀█   █▀▄ █▀▀   █░█ █ █▀▀ █░█   █▀ █▀▀ █░█ █▀█ █▀█ █░░ █
+█▄█ █▀▀   █▄▀ ██▄   █▀█ █ █▄█ █▀█   ▄█ █▄▄ █▀█ █▄█ █▄█ █▄▄ ▄""")
+
+A = "A"
+
+name = input ("wat is je naam?")
+leeftijd = int(input("hoe oud ben je?"))
+
+if leeftijd <18:
+    print(f"Beste {name}, je bent helaas te jong om het spel te spelen!")
+else:
+    print(f"Beste {name}, u bent oud genoeg om het spel te mogen spelen!")
+                                                                                    
+    print(""""                                                                               
+                                                           ###                  
+     &&&&#,((((#####%%%%%%%%&&&&&&&&&&&&&%%%%%%%%%%%%%%%&&&@%#%%%&&% #          
+    %%&&&&&&&&&&&&&&&&&&&&&&&%%%%%#############%#%%%%#%####%&&@#%%%@%           
+    (%&&######%%####%##%##%%#%%%%%#########%###%#%%%%%%###%%@@#%%%%%&&          
+     &&@&&&&&&&&&&&@@@@@@@&&&&&&%######%%%#####@@@%%##@%%%%%%%%%%%%&@%%         
+     ##%%%%%%%%%%%%%#%%%%%%%%%##################@%(##############%&@@@%%%&#     
+        %%%%%%%%%%%%%%%%%%&&%%##@###%%%%%&##%####@&&@%%&*/(&&&&&&@@@&&(         
+                             %%%%%#      *#%    (#@&&@&%@%%&@&&&&@@&            
+                               %#         #(      #@@&%@@&&&%%&@@@@&            
+                               %.        #       %#@@@##%&@&&@&%@@@&%           
+                               %##    (        ,&%&%@@&%&%###%%%&&@@&&          
+                                      ,####,       %%&&&@%%%%&%%&&&@@&&         
+                                                    %%&&%%&@%&@&&&@&@@&&        
+                                                     %&&@&&%@@&@&&&&@@@&&       
+                                                      &&&%@&&&@@&@&&&@&@&%      
+                                                      %&@&&&@&&&@&&&@@@@@&%     
+                                                      %&@&#%%%%%%#%%%%&@@@@     
+                                                      %&@@@@@@@@@@@@@&&&&@&     
+                                                      %%%%%&&%,        %&#   """)
+
+    startvraag = input("Er is een gek die met wapens op leerlingen schiet, hij is op dit moment in de kantine, jij zit in het IT lokaal. Wat ga jij doen?\nA: rent via de nooduitgang naar buiten.\nB: blijft in het lokaal en gaat verstoppen.\n").lower()
+    if startvraag == "a":
+        print("Je rent naar buiten en je stopt met rennen op een gegeven moment, wat ga je doen?\nA: Je belt de politie.\nB: je gaat hem proberen aan te vallen als hij omkijkt.")
+    else:
+        print("Je blijft in het lokaal en gaat je verstoppen, je ziet dat de dader met het wapen gaat kijken of er mensen in je lokaal zitten, wat ga jij doen?\nA: je blijft cool, calm, collective net Selma Omari.\nB: wees dom en gek net famke louise en rent rondjes door het lokaal.")
+
+      eerstelinksroute = input ("Je besluit om je terug te trekken en je gaat in een hoekje huilen, wat besluit je te doen als de schutter naar je toe rent en niet besluit te schieten?\nA: je blijft als een mietje huilen en hoopt dat hij niet op jou gaat schieten.\nB: je besluit om als een gek weg te rennen en riskeert dat hij gaat schieten!\n").lower()
+       if eerstelinksroute == "a":
+        print("Je belt de politie en twijfelt tussen 2 nummers, welk nummer besluit jij te bellen?\nA:112\nB:911")
+       else:
+        print("Je gaat hem proberen aan te vallen wat ga je doen?\nA: een goeie op zijn linker!\nB: een goeie op zijn rechter!")
+
+                eersteabroute = input ("Je gaat hem proberen aan te vallen wat ga je doen?\nA: je gaat hem met de vuist proberen te pakken, zodat je de held van de eeuw kunt gaan worden!\nB: Je besluit toch terug te trekken en je gaat als een mietje in een hoekje huilen.\n").lower() 
+                 if eersteabroute == "a":
+                 print("Je besluit om je terug te trekken en je gaat in een hoekje huilen, wat besluit je te doen als de schutter naar je toe rent en niet besluit te schieten?\nA: je blijft als een mietje huilen en hoopt dat hij niet op jou gaat schieten.\nB: je besluit om als een gek weg te rennen en riskeert dat hij gaat schieten!") 
+                 else:
+                 print ("Je gaat hem met de vuist proberen te pakken, hoe doe je dat?\nA: een goeie op zijn linker\nB: een goeie op zijn rechter")
+
+                   eersteabbroute = input("Je besluit om je terug te trekken en je gaat in een hoekje huilen, wat besluit je te doen als de schutter naar je toe rent en niet besluit te schieten?\n A: jeblijft als een mietje huilen en hoopt dat hij niet op jou gaat schieten.\nB: je besluit om als een gek weg te rennen en riskeert dat hij gaat schieten!\n").lower()
+                   if eersteabbroute == "a":
+                   print("Politie is optijd aangekomen en je bent in veiligheid gebracht en de dader is opgepakt!")
+                   else:
+                   print("je bent ten gevolge van je laatste keuze overleden, R.I.P")
+
+                   startvraagrechts = input ("je blijft in het lokaal en gaat je verstoppen, je ziet dat de dader met het wapen gaat kijker of er mensen in je lokaal zitten, wat ga jij doen?\nA: je blijft cool, calm, collective net Selma Omari.\nB: wees dom en gek net famke louise en rent rondjes door het lokaal.\n").lower() == "a"
+                    if startvraagrechts == "a":
+                  print("je bent cool, calm, collective gebleven net Selma, je ziet dat de shooter uit het lokaal rent, wat ga je doen?\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel.\nB: je gaat spullen verzamelen om op het te gooien of om jezelf te beschermen. ")
+    else:
+        print("je gaat als een gek rondjes rennen, maar je bedenkt je dat het niet het slimste is wat je hebt gedaan, wat ga je nu doen? want de schutter komt steeds dichterbij!\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel.\n B: je gaat spullen verzamelen om op het te gooien of om jezelf te beschermen.")
+        
+
+    startvraagrechtsrechts = input ("je bent cool, calm, collective gebleven net Selma, je ziet dat de shooter uit het lokaal rent, wat ga je doen?\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel.\nB: je gaat spullen verzamelen om op het te gooien of om jezelf te beschermen.\n ").lower() == "a"
+    if startvraagrechtsrechts == "a":
+        print ("je hebt de aanslag overleefd!")
+    else:
+        print ("U bent helaas ten gevolgen van uw beslissingen het niet overleefd!")
+
+    if input("je gaat als een gek rondjes rennen, maar je bedenkt je dat het niet het slimste is wat wat je hebt gedaan wat ga je nu doen? Want de schutter komt dichterbij?\nA: je denkt gewoon masterlijk te zijn en verstopt je onder een tafel. \nB: je gaat spullen verzamelen om op hem te gooien of om jezelf te beschermen.\n ").lower() == "a" :
+        print("U heeft helaas verloren!")
+    else:
+        print("U heeft helaas verloren!")
